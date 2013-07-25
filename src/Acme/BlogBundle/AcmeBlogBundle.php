474f063cb1aa64ab7a1f5591b5a83b98b568fdb7
d1gi <digital_boy@mail.ru>
2013-07-26 01:47:05 +0700

    fix

--- src/Acme/BlogBundle/AcmeBlogBundle.php
+++ src/Acme/BlogBundle/AcmeBlogBundle.php
@@ -3,8 +3,8 @@
 namespace Acme\BlogBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use SmartCore\Bundle\BlogBundle\SmartBlogBundle;
 
-class AcmeBlogBundle extends SmartBlogBundle
+class AcmeBlogBundle extends Bundle
 {
+
 }
